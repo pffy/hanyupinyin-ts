27e28a42506716780c2bedbf7479d193f3cb3398
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2022-05-18 11:03:34 -0700

    Update obj.ts

--- obj.ts
+++ obj.ts
@@ -1,20 +1,38 @@
 /*
  * src      : obj.ts
  * job      : generic TypeScript object
- * git      : https://github.com/pffy/obj-ts
+ * git      : https://github.com/pffy/hanyupinyin-ts
  * author   : The Pffy Authors https://pffy.dev
  * license  : https://opensource.org/licenses/MIT
  */
 
-interface Something {
-  key: string,
-  value: string
-}
+import { IdxHanyuPinyin as idxhp } from "hpdx-ts";
 
-export class SomeObject {
-
-  static readonly derp: Something = {
-    key: 'hello',
-    value: 'world'
-  };
+export class HanyuPinyin {
+  
+  private input: string = '';
+  private text: string = '';
+  
+  setInput(str: string): HanyuPinyin {
+    this.input = str;
+    this.text = str;
+    this.convert();
+    return this;
+  }
+  
+  private convert(): void {
+    for (let h in idxhp.hpdx) {
+      this.text = this.text.replace((new RegExp(h, 'g')), ' ' + idxhp.hpdx[h] + ' ');
+    }
+  
+    this.text = finish(this.text);
+  }
+  
+  private finish(str) {
+    return str.replace((new RegExp('[^\\S\\n]{2,}', 'g')), ' ').trim();
+  }
+  
+  toString(): string {
+    return this.text;
+  }
 }
